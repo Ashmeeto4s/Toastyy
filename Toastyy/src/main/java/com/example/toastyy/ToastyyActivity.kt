24a1a1c78705005de4f15fd489ab22805234dcd3
Ashmeeto4s <ashmeet.arora@original4sure.com>
2021-09-21 00:52:36 +0530

    first commit

---
+++ Toastyy/src/main/java/com/example/toastyy/ToastyyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.toastyy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ToastyyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toastyy)
+    }
+}
